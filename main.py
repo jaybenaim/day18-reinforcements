e29ae6f1b16ea8c0b5d4fd1eba931799c0a70c57
jaybenaim <benaimjacob@gmail.com>
2019-07-25 16:32:52 -0400

    complete

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 def word_counter(string): 
-    words = []
-
-    for word in string:
-        each_word = word.split()
-        words.append(each_word)
+    word_list = []
+    
+    for words in string: 
+        each_word = string.split()
+        if words == None: 
+            return 0  
+        else: 
+            for word in each_word:  
+                word_list.append(word)
         
-    for word in words: 
-        length = len(word)
+        return len(word_list)
+    
 
-    return length 
+    
 
-print(word_counter(['this is a test']) )
+print(word_counter('this is a test'))
+
+print(word_counter("Hello world") )# returns 2
+
+print(word_counter("This is a sentence")) # returns 4
+
+print(word_counter("")) # returns 0
